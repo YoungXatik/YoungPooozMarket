755e75b89837b242d0ed9ad5aeafcf590773844a
POZ <antoivans@gmail.com>
2020-11-09 23:42:16 +0300

    Create discription block

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 const myCollapsible = document.getElementById('navbarSupportedContent')
-console.log(myCollapsible)
 myCollapsible.addEventListener('show.bs.collapse', function () {
     document.getElementById("contacts").classList.remove('offset-5')
-    document.getElementById('contacts').classList.remove('col-2')
-    document.getElementById("contacts").classList.add('col-12');
-    document.getElementById("dropdown-menu").classList.add('col-12');
 })
 myCollapsible.addEventListener('hidden.bs.collapse', function () {
-    document.getElementById('contacts').classList.remove('col-12');
-    document.getElementById('dropdown-menu').classList.remove('col-12');
     document.getElementById('contacts').classList.add('offset-5');
-    document.getElementById('contacts').classList.add('col-2');
+})
 
+
+let mql = window.matchMedia('(min-width: 1200px)');
+mql.addEventListener("change", (e) => {
+    if (e.matches) {
+        document.getElementById('contacts').classList.add('offset-5');
+
+    } else {
+        document.getElementById("contacts").classList.remove('offset-5');
+    }
 })
